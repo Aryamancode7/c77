2f8a02581dc259963711ca16cc7975bfe15a6def
Aryamancode7 <152052779+Aryamancode7@users.noreply.github.com>
2024-01-20 11:56:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("aryaman")
+var First_name="Aryaman";
+console.log(First_name)
+var last_name="Sharma"
+console.log(last_name)
+var full_name=First_name.concat(last_name)
+console.log(full_name)
+var full_name_length=full_name.length
+console.log(full_name_length)
+var name_string="I am Aryaman Sharma."
+console.log(name_string)
+var name_replace=name_string.replace("Sharma","superhero")
+console.log(name_replace)
+var name_capital=First_name.toUpperCase()
+console.log(name_capital)
+var name_small=First_name.toLowerCase()
+console.log(name_small)
+var number=7
+console.log(number)
+var squre_root=Math.sqrt(4)
+console.log(squre_root)
